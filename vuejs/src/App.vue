550540b47edde8a3f76d618a9ca9d062b2c3eb48
george <peraldonamoc@gmail.com>
2021-12-12 01:36:32 -0500

    general data component finished

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Hola"/>
+  <GeneralData title="ACTA DE NACIMIENTO" subtitle="REGRISTRO CIVIL"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GeneralData from './components/GeneralData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GeneralData
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ vuejs/src/components/GeneralData.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="ui container general-data">
+    <div class="title">
+      <h1 class="ui header">
+        {{ title }}
+        <h3 class="sub header">{{ subtitle }}</h3>
+      </h1>
+    </div>
+    <div class="ui huge labels category">
+      <div class="ui label">DATOS GENERALES</div>
+    </div>
+    <div class="ui two column doubling stackable grid container">
+      <div class="column">
+        <div class="ui segment">
+          <form class="ui form">
+            <div class="four wide field">
+              <label>Oficialia N°</label>
+              <input type="number" name="oficialia"/>
+            </div>
+            <div class="four wide field">
+              <label>Libro N°</label>
+              <input type="number" name="libro"/>
+            </div>
+            <div class="four wide field">
+              <label>Acta N°</label>
+              <input type="number" name="acta"/>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="column">
+        <div class="ui segment">
+          <form class="ui form">
+            <div class="field">
+              <label>Departamento</label>
+              <select name="departamentos" id="departamentos" class="ui fluid dropdown">
+                <option value="">Cajamarca</option>
+              </select>
+            </div>
+            <div class="field">
+              <label>Municipio o Delegación</label>
+              <select name="municipio" id="municipio" class="ui fluid dropdown">
+                <option value="">Yonan-Tembladera</option>
+              </select>
+            </div>
+            <div class="field">
+              <label>Localidad</label>
+              <select name="localidad" id="localidad" class="ui fluid dropdown">
+                <option value="">Tembladera</option>
+              </select>
+            </div>
+            <div class="field" id="date">
+                <p class="ui tiny header" style="padding-right: 16px">Fecha de Registro</p>
+                <i class="calendar outline icon large orange"></i>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="btn-continuar">
+      <button class="ui button purple">Continuar<i class="play icon" style="padding-left: 16px"></i></button>
+    </div>
+    <!-- <div class="ui vertical divider" /> -->
+  </div>
+</template>
+
+<style>
+.general-data {
+  padding: 1em;
+  margin-top: 1em;
+}
+.general-data .title {
+  text-align: end;
+  margin-bottom: 3em;
+}
+
+.general-data .btn-continuar {
+  margin-top: 2em;
+  display: flex;
+  margin-right: 10px;
+  justify-content: flex-end;
+}
+
+.general-data .category {
+  padding-left: 1em;
+}
+
+#date {
+  display: flex;
+}
+</style>
+
+<script>
+export default {
+  name: "GeneralData",
+  props: {
+    title: String,
+    subtitle: String,
+  },
+};
+</script>
